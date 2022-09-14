4560bd2f11b0ef10150e18bf3c728ac26eaab056
Franklyn R <franklyn1998@live.com>
2022-09-14 15:34:48 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/github/hadesfranklyn/config/testConfig.java
@@ -0,0 +1,46 @@
+package com.github.hadesfranklyn.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.hadesfranklyn.domain.Categoria;
+import com.github.hadesfranklyn.domain.Produto;
+import com.github.hadesfranklyn.repositories.CategoriaRepository;
+import com.github.hadesfranklyn.repositories.ProdutoRepository;
+
+@Configuration
+public class testConfig implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+	}
+	
+	
+}
