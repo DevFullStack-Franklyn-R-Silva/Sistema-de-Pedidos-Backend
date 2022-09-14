5c7272780e572874b3d12c9807bb57700f71279d
Franklyn R <franklyn1998@live.com>
2022-09-14 18:56:05 -0300

    Main

--- src/main/java/com/github/hadesfranklyn/config/testConfig.java
+++ src/main/java/com/github/hadesfranklyn/config/testConfig.java
@@ -1,5 +1,6 @@
 package com.github.hadesfranklyn.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,22 @@ import com.github.hadesfranklyn.domain.Cidade;
 import com.github.hadesfranklyn.domain.Cliente;
 import com.github.hadesfranklyn.domain.Endereco;
 import com.github.hadesfranklyn.domain.Estado;
+import com.github.hadesfranklyn.domain.ItemPedido;
+import com.github.hadesfranklyn.domain.Pagamento;
+import com.github.hadesfranklyn.domain.PagamentoComBoleto;
+import com.github.hadesfranklyn.domain.PagamentoComCartao;
+import com.github.hadesfranklyn.domain.Pedido;
 import com.github.hadesfranklyn.domain.Produto;
+import com.github.hadesfranklyn.domain.enums.EstadoPagamento;
 import com.github.hadesfranklyn.domain.enums.TipoCliente;
 import com.github.hadesfranklyn.repositories.CategoriaRepository;
 import com.github.hadesfranklyn.repositories.CidadeRepository;
 import com.github.hadesfranklyn.repositories.ClienteRepository;
 import com.github.hadesfranklyn.repositories.EnderecoRepository;
 import com.github.hadesfranklyn.repositories.EstadoRepository;
+import com.github.hadesfranklyn.repositories.ItemPedidoRepository;
+import com.github.hadesfranklyn.repositories.PagamentoRepository;
+import com.github.hadesfranklyn.repositories.PedidoRepository;
 import com.github.hadesfranklyn.repositories.ProdutoRepository;
 
 @Configuration
@@ -40,6 +50,15 @@ public class testConfig implements CommandLineRunner{
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	
 	@Override
@@ -47,34 +66,37 @@ public class testConfig implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+		Estado est3 = new Estado(null, "Alagoas");
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+		Cidade c4 = new Cidade(null, "Arapiraca", est3);
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		est3.getCidades().addAll(Arrays.asList(c4));
 
-		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+		estadoRepository.saveAll(Arrays.asList(est1, est2, est3));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "49247627095", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("999983658", "912365248"));
@@ -86,6 +108,38 @@ public class testConfig implements CommandLineRunner{
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2021 12:00"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2021 19:00"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2021 00:00"),
+				null);
+
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll((Arrays.asList(ped1, ped2)));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 2000.0);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.0);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.0, 1, 800.0);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		;
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 	
 	
---
+++ src/main/java/com/github/hadesfranklyn/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.github.hadesfranklyn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.hadesfranklyn.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
