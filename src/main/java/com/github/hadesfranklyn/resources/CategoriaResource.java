92f8552181479e5e5bd5787f3597b816c9de0fbc
Franklyn R <franklyn1998@live.com>
2022-09-14 14:18:14 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/github/hadesfranklyn/resources/CategoriaResource.java
+++ src/main/java/com/github/hadesfranklyn/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.github.hadesfranklyn.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.hadesfranklyn.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
