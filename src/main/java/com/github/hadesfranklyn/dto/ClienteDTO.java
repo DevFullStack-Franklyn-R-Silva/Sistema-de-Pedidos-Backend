0f3fd92ef51bdc93b6f6f867b8f62962e3c2f212
Franklyn R <franklyn1998@live.com>
2022-09-20 18:54:27 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/github/hadesfranklyn/dto/ClienteDTO.java
+++ src/main/java/com/github/hadesfranklyn/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.github.hadesfranklyn.domain.Cliente;
+import com.github.hadesfranklyn.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
