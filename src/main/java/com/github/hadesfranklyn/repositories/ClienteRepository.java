647d5c4882694dc6a7ace31fe6af0b445f7846dd
Franklyn R <franklyn1998@live.com>
2022-09-20 18:41:14 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/github/hadesfranklyn/repositories/ClienteRepository.java
+++ src/main/java/com/github/hadesfranklyn/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.github.hadesfranklyn.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.hadesfranklyn.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/github/hadesfranklyn/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/github/hadesfranklyn/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.github.hadesfranklyn.domain.Cliente;
 import com.github.hadesfranklyn.domain.enums.TipoCliente;
 import com.github.hadesfranklyn.dto.ClienteNewDTO;
+import com.github.hadesfranklyn.repositories.ClienteRepository;
 import com.github.hadesfranklyn.resources.exceptions.FieldMessage;
 import com.github.hadesfranklyn.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	public ClienteRepository repository;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
