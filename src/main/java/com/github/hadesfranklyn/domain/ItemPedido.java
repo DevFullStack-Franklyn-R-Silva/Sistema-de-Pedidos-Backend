7405954c7748e778db592b6a43c8fed1d09c78d3
Franklyn R <franklyn1998@live.com>
2022-09-29 10:29:47 -0300

    Implementando toString de Pedido

--- src/main/java/com/github/hadesfranklyn/domain/ItemPedido.java
+++ src/main/java/com/github/hadesfranklyn/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.github.hadesfranklyn.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -109,6 +111,22 @@ public class ItemPedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubtotal()));
+		builder.append("\n");
+		
+		return builder.toString();
 	}
 
 }
--- src/main/java/com/github/hadesfranklyn/services/PedidoService.java
+++ src/main/java/com/github/hadesfranklyn/services/PedidoService.java
@@ -33,11 +33,10 @@ public class PedidoService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
-	
-	
-
-	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -48,25 +47,29 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-
+		
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-
+		
 		obj = repository.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-
+		
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
-
+		
 		itemPedidoRepository.saveAll(obj.getItens());
-
+		System.out.println(obj);
+		
+		
 		return obj;
 	}
 }
