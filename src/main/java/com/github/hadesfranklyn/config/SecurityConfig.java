4cd0444551323296aae4c57f2481495859bd6773
Franklyn R <franklyn1998@live.com>
2022-10-04 10:54:34 -0300

    Implementando autenticacao e geracao do token JWT - PARTE 2

--- src/main/java/com/github/hadesfranklyn/config/SecurityConfig.java
+++ src/main/java/com/github/hadesfranklyn/config/SecurityConfig.java
@@ -18,9 +18,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.github.hadesfranklyn.security.JWTAuthenticationFilter;
+import com.github.hadesfranklyn.security.JWTUtil;
+
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -29,15 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
 
-	private static final String[] PUBLIC_MATCHERS = { 
-			"/h2-console/**" 
-	};
+	@Autowired
+	private JWTUtil jwtUtil;
 
-	private static final String[] PUBLIC_MATCHERS_GET = { 
-			"/produtos/**", 
-			"/categorias/**", 
-			"/clientes/**" 
-	};
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/clientes/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
--- src/main/java/com/github/hadesfranklyn/services/UserDetailsServiceImpl.java
+++ src/main/java/com/github/hadesfranklyn/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.github.hadesfranklyn.domain.Cliente;
 import com.github.hadesfranklyn.repositories.ClienteRepository;
 import com.github.hadesfranklyn.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
